590f48012ca7cc0bf9f18605c4cd3cc03486f27a
Toy Reid <toymreid@gmail.com>
2018-09-29 10:28:39 -0400

    Open multiple tabs

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let changeColor = document.getElementById('changeColor');
+const urlList = ["http://www.facebook.com", "http://www.google.com", "http://www.reddit.com"];
 
 chrome.storage.sync.get('color', function(data) {
 	changeColor.style.backgroundColor = data.color;
@@ -8,12 +9,17 @@ chrome.storage.sync.get('color', function(data) {
 
 changeColor.onclick = function(element) {
 	let color = element.target.value;
+
+
+
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.executeScript(
 			tabs[0].id,
 			{code: 'document.body.style.backgroundColor = "' + color + '";'}
 		)
 	})
-	window.open('https://www.google.com');
-
+	for (let index = 0; index < urlList.length; index++) {
+		const element = urlList[index];
+		chrome.tabs.create({"url": element});
+	}
 }
