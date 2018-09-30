62cbc73fee8e9d180eea4cc5f437fc3df1571365
Toy Reid <toymreid@gmail.com>
2018-09-29 21:37:43 -0400

    LOOK AT HER GO

--- options.js
+++ options.js
@@ -50,66 +50,36 @@ function createTime() {
 	var day = document.getElementById('days').value;
 	var timefrom = document.getElementById('timefrom').value;
 	var timeto = document.getElementById('timeto').value;
-	var jsonKey = [];
+	var jsonKeys = [];
 	var processKeys;
 	var week = document.getElementById('week');
 
 	var diff = timeToMin(timeto) - timeToMin(timefrom);
 
-	//chrome.storage.sync.clear();
+	chrome.storage.sync.clear();
 
-	//for (let index = 0; index < 10000; index++) {
 	for (let min = 0; min < diff; min += 30) {
-		jsonKey.push(day + "-" + addStringTime(timefrom, min));
+		jsonKeys.push(day + "-" + addStringTime(timefrom, min));
 	}
-	// for (let url = 3; url < week.length - 1; url++) {
-	// 	chrome.tabs.create({'url': week[url].value});
-	// }
 
-	
 	chrome.storage.sync.get(null, function(result) {
 		processKeys = result;
-	})
 		
-	console.log("Looking for " + jsonKey);
-	chrome.storage.sync.get(jsonKey, function(result) {
-		var json = {};
-		if (result[jsonKey[0]] !== undefined) {
-			json = result[jsonKey[0]];
-			console.log("result = " + JSON.stringify(result));
-			//chrome.storage.remove(jsonKey);
-		}
-		// for (let index = 0; index < 5000; index++) {
-		//console.log(json, jsonKey);
-		// }
-		for (let url = 3; url < week.length - 1; url++) {
-			json[week[url].value] = week[url].value;
-		}
-		// for (let index = 0; index < 5000; index++) {
-		console.log(json, jsonKey);
-			//debugger;
-		// }
-
-		// for (let index = 0; index < 5000; index++) {
-		// 	console.log(typeof(json));
-		// 	console.log(json);
-		// 	// console.log(jsonKey);
-		// }
-		var timeToURL = {};
-		for (let index = 0; index < jsonKey.length; index++) {
-			const time = jsonKey[index];
-			timeToURL[time] = result[jsonKey[index]];
-		}
-		console.log("set ", JSON.stringify(timeToURL));
-		chrome.storage.sync.set(timeToURL, function() {
-			//console.log("Pushed " + jsonKey + ": " + JSON.stringify(json));
-			//debugger;
-			chrome.storage.sync.get(jsonKey, function(r) {
-				for (let index = 0; index < 20000; index++) {
-					console.log(JSON.stringify(r));
+		console.log("before: ", JSON.stringify(processKeys));
+	
+		for (let i = 0; i < jsonKeys.length; i++) {
+			//console.log("jsonKeys = ", jsonKeys[i]);
+			for (let url = 3; url < week.length - 1; url++) {
+				if(processKeys[jsonKeys[i]] === undefined) {
+					processKeys[jsonKeys[i]] = {};
 				}
-			})
-		})
+				processKeys[jsonKeys[i]][week[url].value] = week[url].value;
+			}
+		}
+
+		console.log("after: ", JSON.stringify(processKeys));
+
+		chrome.storage.sync.set(processKeys);
 	})
 }
 
--- popup.js
+++ popup.js
@@ -1,22 +1,46 @@
-let changeColor = document.getElementById('changeColor');
+//let openTabs = document.getElementById('openTabs');
 const urlList = ["http://www.facebook.com", "http://www.google.com", "http://www.reddit.com"];
 
-// chrome.storage.sync.get('color', function(data) {
-// 	changeColor.style.backgroundColor = data.color;
-// 	changeColor.setAttribute('value', data.color);
-// })
-
-changeColor.onclick = function(element) {
-	let color = element.target.value;
-
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.executeScript(
-			tabs[0].id,
-			{code: 'document.body.style.backgroundColor = "' + color + '";'}
-		)
-	})
-	for (let index = 0; index < urlList.length; index++) {
-		const element = urlList[index];
-		chrome.tabs.create({"url": element});
+function zeroFill(number, width) {
+	width -= number.toString().length;
+	if (width > 0) {
+		return new Array(width + (/\./.test( number ) ? 2 : 1)).join('0') + number;
 	}
+	return number + ""; // always return a string
 }
+
+function createTabs() {
+	// let color = element.target.value;
+	var d = new Date();
+	var n = d.getDay(); // 6
+	var mins = (Math.floor(d.getMinutes() / 30)) * 30;
+	var k = n.toString() + "-" + zeroFill(d.getHours(), 2) + ":" + zeroFill(mins, 2);
+
+
+	// debugger;
+	console.log("k = ", k);
+	chrome.storage.sync.get(k, function(result) {
+		for (var url in result[k]) {
+			// debugger;
+			console.log(url);
+			chrome.tabs.create({"url": url});
+		}
+
+		// for (let index = 0; index < urlList.length; index++) {
+		// 	debugger;
+		// 	// const element = urlList[index];
+		// 	// chrome.tabs.create({"url": element});
+		// }
+	})
+
+	// console.log(typeof(n));
+	// // console.log("day = ", n);
+	// // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	// // 	chrome.tabs.executeScript(
+	// // 		tabs[0].id,
+	// // 		{code: 'document.body.style.backgroundColor = "' + color + '";'}
+	// // 	)
+	// // })
+}
+
+document.getElementById('openTabs').addEventListener('click', createTabs);
